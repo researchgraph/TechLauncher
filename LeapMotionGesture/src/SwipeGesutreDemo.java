0f1b6b5222f3ccb25b7c3ad626bb2175beb67888
Andy <admin@ukiyoe.in>
2016-10-03 16:36:42 +1100

    Added gestures demo
    Week9 Works

---
+++ LeapMotionGesture/src/SwipeGesutreDemo.java
@@ -0,0 +1,109 @@
+import java.io.IOException;
+import com.leapmotion.leap.*;
+import com.leapmotion.leap.Gesture.State;
+
+class MyListener extends Listener {
+	public void onInit(Controller contorller) {
+		System.out.println("Initialized");
+	}
+	
+	public void onConnect(Controller controller) {
+		System.out.println("Connect to Motion Sensor");
+		//Initialize the Circle gesture
+		controller.enableGesture(Gesture.Type.TYPE_CIRCLE);
+		controller.enableGesture(Gesture.Type.TYPE_SWIPE);
+		controller.enableGesture(Gesture.Type.TYPE_SCREEN_TAP);
+		controller.enableGesture(Gesture.Type.TYPE_KEY_TAP);
+	}
+	
+	public void onDisconnect(Controller controller) {
+		System.out.println("Motion Sensor Disconnected");
+	}
+	
+	public void onExit(Controller controller) {
+		System.out.println("Exited");
+	}
+	
+	public void onFrame(Controller controller) {
+		Frame frame = controller.frame(0);
+		
+		GestureList gestures = frame.gestures();
+		for (int i = 0; i < gestures.count(); i++) {
+			Gesture gesture = gestures.get(i);
+			
+			switch (gesture.type()) {
+				case TYPE_CIRCLE:
+					CircleGesture circle = new CircleGesture(gesture);
+					
+					String clockwiseness;
+					if (circle.pointable().direction().angleTo(circle.normal()) <= Math.PI/4) {
+						clockwiseness = "clockwise";
+					} else {
+						clockwiseness = "counter-clockwise";
+					}
+					
+					double sweptAngle = 0;
+					if (circle.state() != State.STATE_START) {
+						CircleGesture previous = new CircleGesture(controller.frame(1).gesture(circle.id()));
+						sweptAngle = (circle.progress() - previous.progress()) * 2 * Math.PI;
+					}
+					
+//					System.out.println("Circle ID: " + circle.id()
+//									+ " State: " + circle.state()
+//									+ " Progress: " + circle.progress()
+//									+ " Radius: " + circle.radius()
+//									+ " Angle: " + Math.toDegrees(sweptAngle)
+//									+ " " + clockwiseness);
+					break;
+					
+				case TYPE_SWIPE:
+					SwipeGesture swipe = new SwipeGesture(gesture);
+//					System.out.println("Swipe ID: " + swipe.id()
+//									+ " State: " + swipe.state()
+//									+ " Swipe Position "  + swipe.position()
+//									+ " Direction: " + swipe.direction()
+//									+ " Speed " + swipe.speed());
+					if(swipe.direction().getX()>0){
+						System.out.println("Next Layout");
+					}else{
+						System.out.println("Previous Layout");
+					}
+					break;
+					
+				case TYPE_SCREEN_TAP:
+					ScreenTapGesture screenTap = new ScreenTapGesture(gesture);
+//					System.out.println("Tap ID: " + screenTap.id()
+//									+	" State: " + screenTap.state()
+//									+	" Position: " + screenTap.position()
+//									+	" Direction: " + screenTap.position()
+//									);
+//					System.out.println("TAP");
+					break;
+
+			}
+		}
+	}
+}
+
+
+
+
+
+public class SwipeGesutreDemo {
+	public static void main(String[] args) {
+		MyListener listener = new MyListener();
+		Controller controller = new Controller();
+		
+		controller.addListener(listener);
+		
+		System.out.println("Press enter to quit");
+		
+		try {
+			System.in.read();
+		} catch(IOException e) {
+			e.printStackTrace();
+		}
+		
+		controller.removeListener(listener);
+	}
+}
